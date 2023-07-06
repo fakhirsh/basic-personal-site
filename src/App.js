c9a25e92b4cdd962bc152e4d9e03034e19449ca5
fakhirsh <fakhir.shaheen@gmail.com>
2023-07-06 09:47:31 +0500

    images added, formatting applied at some places

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
 import React from 'react'
+import jupiter from './assets/jupiter.jpg'
 
 const App = () => {
   return (
-    <div className="flex items-center justify-center h-screen">
-      <div className="p-8 bg-white rounded-lg shadow-lg">
-        <h1 className="text-4xl font-bold text-blue-500 mb-4">
+    <div className="flex items-center justify-center h-screenh-full w-full pt-10 md:pt-20">
+      <div className="p-8 bg-white rounded-lg shadow-lg max-w-md w-full border border-blue-500">
+        
+        <img 
+          src={jupiter} 
+          alt="Profile Picture" 
+          className="mx-auto mb-4 rounded-full h-40 w-40" 
+        />
+
+        <h1 className="text-3xl md:text-4xl font-bold text-blue-500 mb-4">
           Fakhir Shaheen
         </h1>
 
@@ -13,23 +21,24 @@ const App = () => {
         </p>
 
         <p className="mb-2">
-          I am a computer science professor. <br/>These days I'm experimenting with app development. <br/> Links to a few of my projects:
+          I teach computer science.<br/>Links to my projects:
         </p>
 
         <ul className="list-disc pl-6 mb-4">
           <li>
-            <a href="https://google.com" className="text-blue-500 hover:text-blue-600">
-              Project 1
+            <a href="https://google.com" className="text-blue-500 hover:text-blue-600 block">
+              <span className="block">Project 1</span>
             </a>
           </li>
           <li>
-            <a href="https://google.com" className="text-blue-500 hover:text-blue-600">
-              Project 2
+            <a href="https://google.com" className="pt-2 text-blue-500 hover:text-blue-600 block">
+              <span className="block">Project 2</span>
             </a>
           </li>
         </ul>
       </div>
     </div>
+
   )
 }
 
